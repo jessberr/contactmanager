b7b8df9fc3ba064670d33bb0657e631c752ee867
Jessica Berrett <jessicaberrett@Jessicas-MacBook-Pro.local>
2018-11-08 20:31:45 -0700

    added contact component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/Contact';
+import Header from './components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header branding="Contact Manager"/>
+        <div className="container">
+        <Contact
+          name="John Doe"
+          email="jdoe@gmail.com"
+          phone="(555) 555-5555"
+        />
+        <Contact
+          name="Karen Philipelli"
+          email="kphil@gmail.com"
+          phone="(444) 444-4444"
+        />
       </div>
+    </div>
     );
   }
 }
+
 
 export default App;
